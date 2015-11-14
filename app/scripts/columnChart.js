b09989dd2a7d23d919c3d2e64f42bd0539309858
Allan Koudal <allan.koudal@gmail.com>
2015-11-14 20:51:03 +0100

    the time variable can now be in any format e.g. Q1-Q4, 2015Q1 etc. The time-range-slider is replaced by a category filter when the time values cannot be casted to numbers.

--- app/scripts/columnChart.js
+++ app/scripts/columnChart.js
@@ -2,10 +2,8 @@
 // instantiates a dashboard, a range slider and a pie chart,
 // passes in the data and draws it.
 function drawDashboard() {
-  //console.log(timeVar, multiVar);
   var dataset = curDataset;
 
-  // //console.log(dataset.role.time[0]);
   document.getElementById('label').innerHTML = dataset.Dataset(0).label;
   document.getElementById('source').innerHTML = dataset.Dataset(0).source;
 
@@ -16,7 +14,6 @@ function drawDashboard() {
   var tbl = dataset.toTable({
     type: "object"
   });
-  ////console.log('table: ', tbl);
 
   // The newly created datatable is used as input to a google visualization dataTable. 0.6 indicates the api-version.
   var data = new google.visualization.DataTable(tbl, 0.6);
@@ -30,13 +27,10 @@ function drawDashboard() {
     type: 'string',
     role: 'style'
   });
-  //console.log("getDistinctValues: ", data.getColumnIndex(getVarLabels(dataset, data).multi));
   // Pivoting the data
   var distinctValues = data.getDistinctValues(data.getColumnIndex(getVarLabels(dataset, data).multi)); // get an array of distinct values in column 0 (possible values for airport)
   var viewColumns = [timeColumnIndex]; // Set the time-column (years) as the first column in the view
   var groupColumns = [];
-
-
   var classificationIndex = data.getColumnIndex(getVarLabels(dataset, data).multi);
 
   // build column arrays for the view and grouping
@@ -47,7 +41,6 @@ function drawDashboard() {
       calc: (function(x) {
         // console.log("calc X: ", x);
         return function(dt, row) {
-          // console.log("year: ", dt.getValue(row, 0), "| airport: ", dt.getValue(row, 1), " | month: ", dt.getValue(row,2), " | value: ", dt.getValue(row,3));
           // return values of C only for the rows where B = distinctValues[i] (passed into the closure via x)
           return (dt.getValue(row, classificationIndex) == x) ? dt.getValue(row, lastColumnIndex) : null;
         }
@@ -68,27 +61,22 @@ function drawDashboard() {
   var view = new google.visualization.DataView(data);
   view.setColumns(viewColumns);
 
-  // console.log("view: ", view);
-  //
-  // console.log('datatable: ', data);
-  //
-  // console.log("view: ", view);
-
-
   // next, we group the view on column A, which gets us the pivoted data
   pivotedData = google.visualization.data.group(view, [0], groupColumns);
-
-  // console.log("pivoted: ", pivotedData);
-
   pivotedData.insertColumn(0, 'number', columnLabels.time);
 
   // copy values from column 1 (old column 0) to column 0, converted to numbers
   for (var i = 0; i < pivotedData.getNumberOfRows(); i++) {
+    var noRangeSlider = 0;
     var val = pivotedData.getValue(i, 1);
-    if (val != '' && val != null) {
+    console.log("val: ", new Number(val).valueOf());
+    if (val != '' && val != null && !isNaN(new Number(val).valueOf())) {
       pivotedData.setValue(i, 0, new Number(val).valueOf());
+    } else {
+      noRangeSlider++;
     }
   }
+
 
   // Calculate how many data-series the pivoted data has.
   if (!series) {
@@ -102,10 +90,6 @@ function drawDashboard() {
   curChartColors = init.colors;
 
 
-  // Visualize the data in a table
-  /*  var table = new google.visualization.Table(document.getElementById('table_div'));
-      table.draw(pivotedData, {});*/
-
   // Use the pivoted dataset for the chart
   var chartData = pivotedData;
 
@@ -113,20 +97,41 @@ function drawDashboard() {
   var dashboard = new google.visualization.Dashboard(
     document.getElementById('dashboard_div'));
 
-  var timePicker = new google.visualization.ControlWrapper({
-    'controlType': 'NumberRangeFilter',
-    'containerId': 'time_picker',
-    'options': {
-      'filterColumnLabel': columnLabels.time,
-      'ui': {
-        'labelStacking': 'vertical',
-        'labelSeparator': ':',
-        'format': {
-          'pattern': '####'
+  if (noRangeSlider) {
+    var timePicker = new google.visualization.ControlWrapper({
+        'controlType': 'CategoryFilter',
+        'containerId': 'time_picker',
+        'options': {
+            'filterColumnIndex': timeColumnIndex-1,
+            'ui': {
+                'labelStacking': 'vertical',
+                'allowTyping': true,
+                'allowMultiple': true,
+                'sortValues': false,
+                'cssClass': 'styled-category-filter',
+                'allowNone': false
+            },
+        },
+        // 'state': {
+        //     'selectedValues': ['2014']
+        // }
+    });
+  } else {
+    var timePicker = new google.visualization.ControlWrapper({
+      'controlType': 'NumberRangeFilter',
+      'containerId': 'time_picker',
+      'options': {
+        'filterColumnLabel': columnLabels.time,
+        'ui': {
+          'labelStacking': 'vertical',
+          'labelSeparator': ':',
+          'format': {
+            'pattern': '####'
+          }
         }
       }
-    }
-  });
+    });
+  }
 
   // Create a  chart, passing some options
   var chart = new google.visualization.ChartWrapper({
@@ -147,7 +152,6 @@ function drawDashboard() {
   // Establish dependencies, declaring that timePicker, monthPicker and airportPicker drives 'pieChart',
   // so that the pie chart will only display entries that are let through these filters.
   dashboard.bind(timePicker, chart);
-  ////console.log("draw chart type: ", chartType);
   // Draw the dashboard.
   dashboard.draw(chartData);
 
